e21e830a17ecea44bbab90bf348eb623aeaa78e4
TinzarTun <tinzartun2002@gmail.com>
2026-02-01 12:09:38 +0630

    jquery dialog test

---
+++ dialog test (jquery)/js/master.js
@@ -0,0 +1,11 @@
+// $(".open-dialog").on("click", function() {
+//     $(".overlay").slideDown(1000);
+// });
+
+// $(".close-dialog").on("click", function() {
+//     $(".overlay").slideUp(1000);
+// });
+
+$(".dialog").on("click", function () {
+  $(".overlay").slideToggle(1000);
+});
